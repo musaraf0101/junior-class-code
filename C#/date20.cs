87ff3d5fc707fdbfba30684f48dded7722537b00
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-10-22 18:46:29 +0530

    question added

---
+++ C#/date20.cs
@@ -0,0 +1,31 @@
+   //proram 3 type
+            /*
+            sequancel -> example : grade 1 to 12
+            selection -> example :  
+            loop -> 
+            */
+        //while
+
+            int a = 0;
+            while (a <= 10)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+
+        //do-while
+
+            int a = 15;
+            do
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            while (a < 10);
+            
+        //for
+
+        for(int i = 15; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
---
+++ example.java
@@ -0,0 +1,6 @@
+public class example {
+    public static void main(String[] args) {
+        final int a=10;
+         System.out.println(a);
+    }
+}
